67550d710638384a6ee710b2982e18d803dbb72e
DEGKOF <varchangedegueno@gmail.com>
2025-11-21 12:37:12 +0100

    Update version 5 add manifest file

--- resources/views/apropos.blade.php
+++ resources/views/apropos.blade.php
@@ -76,7 +76,16 @@
 <section class="section section-dark">
     <div class="container">
         <div class="manifeste-content">
-            <span class="section-badge light">Manifeste Fondateur</span>
+            <a href="{{ asset('documents/LE MANIFESTE MBRC.pdf') }}"
+                class="btn btn-primary"
+                download="MBRC-Manifeste.pdf">Manifeste Fondateur
+                <svg class="icon" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        d="M12 10v6m0 0l-3-3m3 3l3-3m2 8H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z">
+                    </path>
+                </svg>
+            </a>
+            {{-- <span class="section-badge light">Manifeste Fondateur</span> --}}
             <h2 class="section-title light">Pourquoi cette maison d'idées ?</h2>
 
             <div class="manifeste-grid">
